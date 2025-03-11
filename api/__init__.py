9b18fe91e92376cbeed57f6b88131fed3f3af971
Elmersperry <Elmersperry@mail.ru>
2025-03-11 20:28:37 +0300

    dfjkhfgrukhulih

---
+++ api/__init__.py
@@ -0,0 +1,4 @@
+from .vacancies_api import get_vacancies
+from .courses_api import get_courses
+from .weather_api import get_weather
+from .keyboards import simple_keyboard
--- api/vacancies_api.py
+++ api/vacancies_api.py
@@ -1,2 +1,7 @@
 def get_vacancies():
-    pass
+    data = [
+        {"name": "Python_1", "salary": "100000-150000", "created_at": "12.12.2022", "url": "https://hh.ru/vacancy/118199266"},
+        {"name": "Python_2", "salary": "100000-200000", "created_at": "12.12.2022", "url": "https://hh.ru/vacancy/118199266"},
+        {"name": "Python_3", "salary": "100000-250000", "created_at": "12.12.2022", "url": "https://hh.ru/vacancy/118199266"}
+    ]
+    return data
